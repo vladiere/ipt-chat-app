f5850e6abfec220a5cf8050c5ea526b9e99aa564
vladiere <ruthakaza@gmail.com>
2023-12-07 20:44:16 +0800

    initial commit

--- backend/main.js
+++ backend/main.js
@@ -9,12 +9,20 @@ const refreshRoute = require('./routes/refresh.route');
 const app = express();
 const PORT = process.env.PORT || 4300;
 const http = require('http').createServer(app);
+const allowedOrigins = ['http://localhost:9000'];
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors({
-  origin: "*"
-}));
+
+const corsOptions = {
+  origin: (origin: any, callback: any) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed origin'));
+    }
+  }
+}
 
 app.get('/', (req, res) => {
   return res.status(200).json({ message: 'OK' });
